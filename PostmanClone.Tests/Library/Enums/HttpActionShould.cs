ed8386760fd4ad5db6515fba63994a7942b1b66a
Adrian Puscasu <adi.puscasu@gmail.com>
2024-05-04 15:52:41 +0300

    adds test project

---
+++ PostmanClone.Tests/Library/Enums/HttpActionShould.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using PostmanClone.Library.Enums;
+
+namespace PostmanClone.Tests.Library.Enums;
+public class HttpActionShould
+{
+    [Fact]
+    public void BeOfTypeEnum()
+    {
+        // Arrange
+        object obj = HttpAction.Get;
+
+        // Act
+        bool isEnum = obj is HttpAction;
+
+        // Assert
+        isEnum.Should().BeTrue();
+    }
+
+    [Fact]
+    public void EnumHasGet()
+    {
+        // Act
+        bool isDefined = Enum.IsDefined(typeof(HttpAction), "Get");
+
+        // Assert
+        isDefined.Should().BeTrue();
+    }
+}
